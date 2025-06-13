faeaf9e3f10ff88c90bb29bf286a06ea815914f8
MyoThet <mr.myothet@gmail.com>
2025-06-13 21:13:27 +0700

    LSP principle

---
+++ ConsoleApp/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Engine
+    {
+
+    }
+
+    public class DiesalEngine : Engine
+    {
+        public void start()
+        {
+            Console.WriteLine("Diesel engine started");
+        }   
+    }
+
+    public class SolarEngine : Engine
+    { 
+        public void start()
+        {
+            throw new NotImplementedException("Solar engine cannot start");
+        }
+    }
+}
